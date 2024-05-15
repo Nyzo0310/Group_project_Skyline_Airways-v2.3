8381576224bd385ceec594e9affa6777d007136b
Earl Sepida <earlsepida63@gmail.com>
2024-05-15 14:39:52 +0800

    Boarding Pass UI

---
+++ Boarding_pass.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boarding Pass</title>
+    <link rel="stylesheet" href="/css/boarding_pass.css">
+</head>
+<body>
+    <div class="boarding-pass">
+        <div class="header">
+            <div class="logo">
+                <img src="/assets/images/logo.jpg" alt="Airplane Logo">
+            </div>
+            <div class="airline-name">
+                <h1>SKYLINE AIRWAYS</h1>
+                <p>To Travel Safe Is Our Deal!</p>
+            </div>
+            <div class="boarding-pass-label">
+                <p>BOARDING PASS</p>
+            </div>
+        </div>
+        <div class="passenger-info">
+            <div class="section">
+                <p>PASSENGER NAME</p>
+                <h2>SEPIDA, EARL</h2>
+            </div>
+            <div class="section">
+                <p>FLIGHT NO.</p>
+                <h2>A5-3841</h2>
+            </div>
+            <div class="section">
+                <p>SEAT</p>
+                <h2>10A</h2>
+            </div>
+            <div class="section">
+                <p>GATE</p>
+                <h2>14A</h2>
+            </div>
+        </div>
+        <div class="boarding-time">
+            <p>BOARDING TIME</p>
+            <h2>4:00 PM - AUG 20, 2020</h2>
+        </div>
+        <div class="barcode">
+            <img src="/assets/images/barcode.gif" alt="Barcode">
+        </div>
+        <div class="footer">
+            <div class="section">
+                <p>FROM</p>
+                <h2>CAGAYAN DE ORO</h2>
+            </div>
+            <div class="section">
+                <p>TO</p>
+                <h2>BORACAY</h2>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
